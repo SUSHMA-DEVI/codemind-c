493f9fd25c2b667f92bcc8bd204348b6d64306be
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-05-24 20:06:59 +0530

    #include<stdio.h> int main() {     int i,j,rows,col,a[10][10],sum;     scanf("%d",&i);     for(rows=0;rows<i;rows++)     {         for(col=0;col<2;col++)         {             scanf("%d",&a[rows][col]);         }     }
        for(rows=0;rows<i;rows++)
        {
            sum=0;
             for(col=0;col<2;col++)
             {
                 sum=sum+a[rows][col];
             }
             printf("%d
    ",sum);
        }
        return 0;
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,rows,col,a[10][10],sum;
+    scanf("%d",&i);
+    for(rows=0;rows<i;rows++)
+    {
+        for(col=0;col<2;col++)
+        {
+            scanf("%d",&a[rows][col]);
+        }
+    }
+    
+    for(rows=0;rows<i;rows++)
+    {
+        sum=0;
+         for(col=0;col<2;col++)
+         {
+             sum=sum+a[rows][col];
+         }
+         printf("%d
+",sum);
+    }
+    return 0;
+}
