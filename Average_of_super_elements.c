90a8c2a1ff950c9a9f54c23a83dfe80671d6743c
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-06-28 15:22:11 +0530

    #include<stdio.h> int main() {     int n,i,j;     float res=0,s=0,sum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         int c=0,x=a[i];         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==x)         {             sum+=a[i];             s++;             a[i]=0;         }     }     if(s==0)     {         printf("-1");     }     else     {         res=sum/s;         printf("%.2f",res);     }
    }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    float res=0,s=0,sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        int c=0,x=a[i];
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==x)
+        {
+            sum+=a[i];
+            s++;
+            a[i]=0;
+        }
+    }
+    if(s==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        res=sum/s;
+        printf("%.2f",res);
+    }
+    
+}
