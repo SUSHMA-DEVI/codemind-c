cbc5d53152901ff86fa04dc17ea40ef70776fef9
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-06-29 21:52:38 +0530

    #include<stdio.h> int pal(int n) {     int rev=0,r;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     return rev; } int main() {     int n,c=0;     scanf("%d",&n);     //scanf("%d",&s);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]==pal(a[i]))         {             c++;         }     }     printf("%d",c); }

---
+++ Count_palindromes.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int pal(int n)
+{
+    int rev=0,r;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    return rev;
+}
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    //scanf("%d",&s);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==pal(a[i]))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
