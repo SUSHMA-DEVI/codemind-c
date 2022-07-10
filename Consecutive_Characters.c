a1605c561324042a77fde375c7d7f35043ca8334
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-07-10 09:11:51 +0530

    #include<stdio.h> #include<string.h> int main() {     int i;     int c=0,c1=0;     char s[100];     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==s[i+1])         {             c++;         }         else         {             if(c1<c)             {                 c1=c;             }             c=0;         }     }     if(c1<c)     {         c1=c;     }     printf("%d",c1+1); }

---
+++ Consecutive_Characters.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    int c=0,c1=0;
+    char s[100];
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==s[i+1])
+        {
+            c++;
+        }
+        else
+        {
+            if(c1<c)
+            {
+                c1=c;
+            }
+            c=0;
+        }
+    }
+    if(c1<c)
+    {
+        c1=c;
+    }
+    printf("%d",c1+1);
+}
